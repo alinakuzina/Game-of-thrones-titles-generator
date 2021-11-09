95bbe7ae026a7eae392abe41d48cd3b2de97643c
alinakuzina <kuzina.lg.4.1@gmail.com>
2021-11-09 12:06:14 +0100

    Add new game

--- script.js
+++ script.js
@@ -262,6 +262,29 @@ let newTern = function () {
     });
 };
 
+let newGame = function () {
+  document
+    .querySelector(".btn-new-game")
+    .addEventListener("click", function (e) {
+      document.querySelector(".next-option-hidden").classList.add("hidden");
+      sectionCardConteiner.classList.add("hidden");
+      inputNumber.value = "";
+
+      namesOfPlayers = [];
+      arrayOfCards = [0, 1, 2, 3, 4, 5];
+      numberOfPlayer = 0;
+      randomCards = [];
+
+      for (let i = 0; i < numberOfPlayer; i++) {
+        document.querySelector(`.input-name-${i}`).value = "";
+      }
+      document.querySelector(".type-names").innerHTML = "";
+      document.querySelector(".btn-submit-number").classList.toggle("hidden");
+      document.querySelector(".btn-start").classList.toggle("hidden");
+      startConteiner.classList.remove("hidden");
+    });
+};
+
 document
   .querySelector(".btn-submit-number")
   .addEventListener("click", function (e) {
@@ -290,303 +313,4 @@ document
 
 startCards();
 newTern();
-
-/*
-
-let namesOfPlayers = [];
-
-let arrayOfCards = [0, 1, 2, 3, 4, 5];
-
-let numberOfPlayer = 0;
-
-let showSectionNumber = 0;
-
-let way = 0;
-
-let randomNum = function (min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
-
-let cardDrowFunc = function () {
-  if (namesOfPlayers.length === 0) {
-    for (let i = 0; i < numberOfPlayer; i++) {
-      console.log(document.querySelector(`.input-name-${i}`).value);
-      namesOfPlayers.push(document.querySelector(`.input-name-${i}`).value);
-    }
-  }
-
-  arrayOfCards = [0, 1, 2, 3, 4, 5];
-
-  numberOfPlayer = 0;
-  randomCards = [];
-
-  numberOfPlayer = inputNumber.value;
-
-  for (let i = 0; i < numberOfPlayer; i++) {
-    let rendNum = randomNum(0, arrayOfCards.length);
-    randomCards.push(arrayOfCards[rendNum]);
-    arrayOfCards.splice(rendNum, 1);
-  }
-
-  let newHtml = "";
-
-  let NameOptions = function () {
-    let optionHtml = "";
-    for (let i = 0; i < namesOfPlayers.length; i++) {
-      optionHtml += `<option value=${namesOfPlayers[i]}>${namesOfPlayers[i]}</option>`;
-    }
-    return optionHtml;
-  };
-  let finish = ` <div class='container-btn-finish'>
-    <p class='error-massege-in-card error-finish'></p>
-    <button class="btn-finish">Finish</button></div>`;
-
-  for (let i = 0; i < randomCards.length; i++) {
-    newHtml += `
-      <div class="card-box card-${randomCards[i]}" >
-        <img
-          class="img-card img-${randomCards[i]}"
-          src="${arrayOfCardInfo[randomCards[i]][1]}"
-          alt="Master of wispers"
-        />
-        <div class="cars-text ">
-          <p class="header-title"><strong>${
-            arrayOfCardInfo[randomCards[i]][2]
-          }</strong></p>
-          <p class="support"><strong>Supports:</strong>${
-            arrayOfCardInfo[randomCards[i]][3]
-          }</p>
-          <p class="Rivals">
-            <strong>Rivals:</strong> ${arrayOfCardInfo[randomCards[i]][4]}
-          </p>
-          <p class="text">
-          ${arrayOfCardInfo[randomCards[i]][5]}
-          </p>
-          <p><i>${arrayOfCardInfo[randomCards[i]][6]}</i></p>
-          <p class='error-massege-in-card error-massege-in-card-${
-            randomCards[i]
-          }'></p>
-      
-          <div class='new-text new-text-${randomCards[i]}'></div>
-          <div class='name-select name-select-${randomCards[i]}'>
-          <select class='input-${randomCards[i]}'>
-          ${NameOptions()}
-          </select>
-          <button class='btn-submit btn-submit-${
-            randomCards[i]
-          }'>Submit</button>
-          </div>
-        </div>
-      </div>
-        </div>
-      </div>`;
-  }
-  sectionCardConteiner.innerHTML = newHtml + finish;
-  document.querySelector(`.img-${randomCards[randomCards.length - 1]}`).onload =
-    function () {
-      sectionCardConteiner.classList.toggle("hidden");
-      startConteiner.classList.toggle("hidden");
-    };
-  return randomCards;
-};
-
-let startCards = function () {
-  return new Promise((resolve, reject) => {
-    btnStart.addEventListener("click", function (e) {
-      e.preventDefault();
-      let randomCardsFromFunc = cardDrowFunc();
-      resolve(randomCardsFromFunc);
-    });
-  });
-};
-
-let increase = function (randomCards) {
-  for (let i = 0; i < randomCards.length; i++) {
-    console.log(randomCards[i]);
-    document
-      .querySelector(`.card-${randomCards[i]}`)
-      .addEventListener("click", function () {
-        document
-          .querySelector(`.card-${randomCards[i]}`)
-          .classList.toggle("increase");
-
-        randomCards
-          .filter((elem, index) => index != i)
-          .forEach((elem) =>
-            document.querySelector(`.card-${elem}`).classList.remove("increase")
-          );
-      });
-    document.querySelectorAll(`.name-select`).forEach((element) =>
-      element.addEventListener("click", function (e) {
-        e.stopPropagation();
-      })
-    );
-  }
-  return randomCards;
-};
-
-let submitData = function (randomCards) {
-  for (let i = 0; i < randomCards.length; i++) {
-    document
-      .querySelector(`.btn-submit-${randomCards[i]}`)
-      .addEventListener("click", function (e) {
-        e.preventDefault();
-        if (document.querySelector(`.input-${randomCards[i]}`).value === "") {
-          document.querySelector(
-            `.error-massege-in-card-${randomCards[i]}`
-          ).innerHTML = "Write your name";
-        } else {
-          console.log(document.querySelector(`.btn-submit-${randomCards[i]}`));
-          console.log(document.querySelector(`.input-${randomCards[i]}`).value);
-
-          let nameOfGamer = document.querySelector(
-            `.input-${randomCards[i]}`
-          ).value;
-          document
-            .querySelector(`.name-select-${randomCards[i]}`)
-            .classList.toggle("hidden");
-          document.querySelector(
-            `.new-text-${randomCards[i]}`
-          ).innerHTML = `${nameOfGamer}`;
-
-          document
-            .querySelector(`.card-${randomCards[i]}`)
-            .classList.add("hidden");
-        }
-      });
-  }
-  return randomCards;
-};
-
-let finish = function (randomCards) {
-  document.querySelector(".btn-finish").addEventListener("click", function () {
-    if (namesOfPlayers.length !== randomCards.length) {
-      document.querySelector(`.error-finish`).innerHTML =
-        "Add all player's Name";
-    } else {
-      for (let j = 0; j < randomCards.length; j++) {
-        document
-          .querySelector(`.card-${randomCards[j]}`)
-          .classList.remove("increase");
-      }
-
-      document.querySelector(`.error-finish`).innerHTML = "";
-      for (let i = 0; i < randomCards.length; i++) {
-        document.querySelector(
-          `.error-massege-in-card-${randomCards[i]}`
-        ).innerHTML = "";
-        document
-          .querySelector(`.card-${randomCards[i]}`)
-          .classList.toggle("hidden");
-      }
-      document
-        .querySelector(".container-btn-finish")
-        .classList.toggle("hidden");
-    }
-  });
-  return randomCards;
-};
-
-// let obsorverFunc = function (randomCards) {
-//   const observer = new MutationObserver(function () {
-//     console.log("callback that runs when observer is triggered");
-//     let numbOftrue = 0;
-//     console.log(document.querySelector(`.card-${randomCards[0]}`));
-//     for (let i = 0; i < randomCards.length; i++) {
-//       if (
-//         document
-//           .querySelector(`.card-${randomCards[i]}`)
-//           .classList.contains("hidden")
-//       ) {
-//         numbOftrue++;
-//       }
-//     }
-//     console.log(numbOftrue, randomCards.length);
-//     if (randomCards.length === numbOftrue) {
-//       console.log("all is hidden");
-//       numbOftrue = 0;
-//       for (let j = 0; j < randomCards.length; j++) {
-//         document
-//           .querySelector(`.card-${randomCards[j]}`)
-//           .classList.remove("increase");
-//       }
-
-//       for (let i = 0; i < randomCards.length; i++) {
-//         document.querySelector(
-//           `.error-massege-in-card-${randomCards[i]}`
-//         ).innerHTML = "";
-//         document
-//           .querySelector(`.card-${randomCards[i]}`)
-//           .classList.toggle("hidden");
-//       }
-//     }
-//     return namesOfPlayers;
-//   });
-//   const elementToObserve = document.querySelector(".section-cards");
-//   observer.observe(elementToObserve, { subtree: true, childList: true });
-// };
-
-document
-  .querySelector(".btn-submit-number")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    if (
-      inputNumber.value === "" ||
-      inputNumber.value < 3 ||
-      inputNumber.value > 6
-    ) {
-      let errorMes = "Please write number of players correct (3-6 players)";
-      document.querySelector(".error-massege").innerHTML = errorMes;
-    } else {
-      document.querySelector(".error-massege").innerHTML = "";
-
-      document.querySelector(".btn-submit-number").classList.toggle("hidden");
-      document.querySelector(".btn-start").classList.toggle("hidden");
-
-      numberOfPlayer = inputNumber.value;
-      let textLine = ``;
-      for (let i = 0; i < numberOfPlayer; i++) {
-        textLine += `<input class="input-name input-name-${i}" type="text" placeholder="Your name" />`;
-        document.querySelector(".type-names").innerHTML = textLine;
-      }
-    }
-  });
-
-let newTern = async function () {
-  document
-    .querySelector(".btn-new-tern")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      way = 1;
-      let randomCardsFromFunc = cardDrowFunc();
-      sectionCardConteiner.classList.toggle("hidden");
-      startConteiner.classList.toggle("hidden");
-      resolve(randomCardsFromFunc);
-    });
-};
-
-let newTernProm = function () {
-  newTern()
-    .then((x) => increase(x))
-    .then((x) => submitData(x))
-    .then((x) => finish(x))
-    .then((x) => newTern(x));
-};
-
-if ((way = 0)) {
-  startCards()
-    .then((x) => increase(x))
-    .then((x) => submitData(x))
-    .then((x) => finish(x))
-    .then((x) => newTern(x));
-} else {
-  newTern()
-    .then((x) => increase(x))
-    .then((x) => submitData(x))
-    .then((x) => finish(x));
-}
-
-
-
-
-*/
+newGame();
